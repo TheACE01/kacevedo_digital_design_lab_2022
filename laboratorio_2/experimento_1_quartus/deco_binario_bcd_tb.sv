f0c3c69971f8e130cb98342376a846ec7142cf4f
Kevin Acevedo <keviinar51@gmail.com>
2022-08-29 00:49:24 -0600

    Finalización del modulo Testbench para el diseño del decodificador
    Se ha terminado de escribir el código para probar el diseño del módulo de diseño, usando varias combinaciones de entradas y el simulador Modelsim-Altera para visualizar los cambios en las salidas.

--- laboratorio_2/experimento_1_quartus/deco_binario_bcd_tb.sv
+++ laboratorio_2/experimento_1_quartus/deco_binario_bcd_tb.sv
@@ -4,10 +4,66 @@ module deco_binario_bcd_tb();
 	// Definiendo las variables que se necesitan para probar el módulo
 	logic A3, A2, A1, A0, S4, S3, S2, S1, S0;
   
-	// Creando una instancia del módulo diseñado
-	deco_binario_bcd modulo(A3, A2, A1, A0, S4, S3, S2, S1, S0);
+	// Creando una instancia del módulo diseñado (Device Under Test)
+	deco_binario_bcd DUT(A3, A2, A1, A0, S4, S3, S2, S1, S0);
 	
 	// Creando un bloque de código que permita cambiar valores lógicos
 	initial begin
-	end
+		// Probando para distintas combinaciones en la entarda
+		A3 = 0;
+		A2 = 0;
+		A1 = 0;
+		A0 = 0;
+		
+		#5 // Retardo manual de 5 unidades de tiempo
+		
+		A3 = 0;
+		A2 = 0;
+		A1 = 0;
+		A0 = 1;
+		
+		#5
+		
+		A3 = 0;
+		A2 = 0;
+		A1 = 1;
+		A0 = 0;
+		
+		#5
+		
+		A3 = 0;
+		A2 = 1;
+		A1 = 1;
+		A0 = 1;
+		
+		#5
+		
+		A3 = 1;
+		A2 = 0;
+		A1 = 0;
+		A0 = 0;
+		
+		
+		#5
+		
+		A3 = 1;
+		A2 = 0;
+		A1 = 0;
+		A0 = 1;
+		
+		#5
+		A3 = 1;
+		A2 = 1;
+		A1 = 0;
+		A0 = 0;
+		
+		#5
+		
+		A3 = 1;
+		A2 = 1;
+		A1 = 1;
+		A0 = 1;
+		#5;
+
+	end 
 endmodule
