5b112805a41cc8b0365b25de2bd190b1aabe61da
Kevin Acevedo <keviinar51@gmail.com>
2022-08-29 00:36:05 -0600

    Primera finalización del diseño del decodificador
    Se ha usado un modelo de comportamiento para definir los valores de las variables de salida del decodificador

--- laboratorio_2/experimento_1_quartus/deco_binario_bcd.sv
+++ laboratorio_2/experimento_1_quartus/deco_binario_bcd.sv
@@ -1,7 +1,16 @@
 module deco_binario_bcd (A3, A2, A1, A0, S4, S3, S2, S1, S0);
-
+	// Entradas del modulo
   input logic A3, A2, A1, A0;
+	// Salidas del modulo
   output logic S4, S3, S2, S1, S0;
   
+  // Modelo de comportamiento para cada una de las variables de salida
+  // S es una sola variable de salida pero con sus bits separados en variables
+  // COn S4 siendo el MSB
+  assign S4 = (A3 & A2) | (A3 & A1);
+  assign S3 = (A3 & ~A2 & ~A1);
+  assign S2 = (~A3 & A2) | (A2 & A1);
+  assign S1 = (A3 & A2 & ~A1) | (~A3 & A1);
+  assign S0 = A0;
   
 endmodule
