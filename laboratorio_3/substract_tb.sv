fad701d912b67f681ccc8edc52cc98b4bde3f32f
Kevin Acevedo <keviinar51@gmail.com>
2022-09-20 22:48:10 -0600

    No update

--- laboratorio_3/substract_tb.sv
+++ laboratorio_3/substract_tb.sv
@@ -1,16 +1,16 @@
 // This is the testbench module for testing the N bit multiplier
 
 `timescale 1ns / 1ps
-module multiplier_tb();
+module substract_tb();
 
 	// Defining the instance signals
 	parameter N = 4;
 	logic [N-1:0] A, B;
-	logic [(2*N)-1:0] S;
+	logic [N-1:0] Z;
 
 
 	// Instance of the Unit Under Test (UUD)
-	multiplier #(.N(N)) uut(
+	substract #(.N(N)) uut(
 	.A(A),
 	.B(B),
 	.Z(Z));
@@ -18,7 +18,7 @@ module multiplier_tb();
 	// Testing the design module functinality
 	initial begin
 	A = 4'b1100;
-	B = 4'b1011;
+	B = 4'b1111;
 	# 10;
 	A = 4'b1001;
 	B = 4'b10;
