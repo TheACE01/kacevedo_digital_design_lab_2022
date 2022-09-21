fdbf7ab32807a06bfa1dfec42fd12c0d15fd262a
Kevin Acevedo <keviinar51@gmail.com>
2022-09-20 21:37:20 -0600

    New Modules Added
    Operations AND, OR, XOR and Multiply Added to Quartus

---
+++ laboratorio_3/op_and.sv
@@ -0,0 +1,16 @@
+/*
+
+Module: AND Operation
+Description: This module performs AND operation for N-bit inputs.
+
+*/
+
+module op_and #(parameter N = 4)(A, B, Z);
+	input [N-1:0] A, B;
+	output [N-1:0] Z;
+	
+	// Using the HDL operators
+	assign Z = A & B;
+	
+endmodule
+	
